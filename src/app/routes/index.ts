089807e6f3b2051c8f6df37d2e5c5b0a9d08dfd6
fahimmuntacher <muntacherfahim234@gmail.com>
2026-02-27 15:27:41 +0600

    doctor schedule api create

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -3,12 +3,16 @@ import { specialityRouter } from "../modules/speciality/speciality.routes";
 import { UserRoutes } from "../modules/user/user.routes";
 import { DoctorRoutes } from "../modules/doctor/doctor.routes";
 import { authRotuer } from "../modules/auth/auth.routes";
+import { scheduleRoutes } from "../modules/schedule/schedule.routes";
+import { DoctorScheduleRoutes } from "../modules/doctorSchedule/doctorSchedule.routes";
 
 const router = Router();
 
-router.use("/specialities", specialityRouter)
-router.use("/auth", authRotuer)
-router.use("/users", UserRoutes)
-router.use("/doctors", DoctorRoutes)
+router.use("/specialities", specialityRouter);
+router.use("/auth", authRotuer);
+router.use("/users", UserRoutes);
+router.use("/doctors", DoctorRoutes);
+router.use("/schedules", scheduleRoutes);
+router.use("/doctor-schedules", DoctorScheduleRoutes);
 
 export const IndexRoutes = router;
