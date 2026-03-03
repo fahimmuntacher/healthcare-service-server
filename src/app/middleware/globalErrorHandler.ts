e6779ba18445d456d72f6e0402c4ea672b8c0a66
fahimmuntacher <muntacherfahim234@gmail.com>
2026-03-04 00:41:05 +0600

    delete uploaded files from global error handler utils add

--- src/app/middleware/globalErrorHandler.ts
+++ src/app/middleware/globalErrorHandler.ts
@@ -6,7 +6,7 @@ import status from "http-status";
 import z from "zod";
 import { TErrorSources } from "../interfaces/error.interfaces";
 import { handleZodErrors } from "../errorHelpers/handleZodErrors";
-import { deleteFileFromCloudinary } from "../config/cloudinary.config";
+import { deleteUploadedFilesFromGlobalErrorHandler } from "../utils/deleteUploadedFilesFromGlobalErrorHandler";
 
 export const globalErrorHandler = async (
   err: any,
@@ -18,14 +18,16 @@ export const globalErrorHandler = async (
     console.log(err);
   }
 
-  if (req.file) {
-    await deleteFileFromCloudinary(req.file?.path);
-  }
+  // if (req.file) {
+  //   await deleteFileFromCloudinary(req.file?.path);
+  // }
 
-  if (req.files && Array.isArray(req.files) && req.files.length > 0) {
-    const imageUrls = req.files.map((file) => file.path);
-    await Promise.all(imageUrls.map((url) => deleteFileFromCloudinary(url)));
-  }
+  // if (req.files && Array.isArray(req.files) && req.files.length > 0) {
+  //   const imageUrls = req.files.map((file) => file.path);
+  //   await Promise.all(imageUrls.map((url) => deleteFileFromCloudinary(url)));
+  // }
+
+  await deleteUploadedFilesFromGlobalErrorHandler(req);
 
   const errorSource: TErrorSources[] = [];
   let statusCode: number = status.INTERNAL_SERVER_ERROR;
