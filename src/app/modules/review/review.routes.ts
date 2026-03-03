a869f08b10843829c67adddc7e2a686fd16916a8
fahimmuntacher <muntacherfahim234@gmail.com>
2026-03-04 02:10:06 +0600

    prescirption and review api created

---
+++ src/app/modules/review/review.routes.ts
@@ -0,0 +1,30 @@
+
+import express from 'express';
+
+import { validateRequest } from '../../middleware/validateRequest';
+import { ReviewController } from './review.controller';
+import { ReviewValidation } from './review.validation';
+import { checkAuth } from '../../middleware/checkAuth';
+import { Role } from '../../../generated/prisma/enums';
+
+const router = express.Router();
+
+router.get('/', ReviewController.getAllReviews);
+
+router.post(
+    '/',
+    checkAuth(Role.PATIENT),
+    validateRequest(ReviewValidation.createReviewZodSchema),
+    ReviewController.giveReview
+);
+
+router.get('/my-reviews', checkAuth(Role.PATIENT, Role.DOCTOR), ReviewController.myReviews);
+
+router.patch('/:id', checkAuth(Role.PATIENT), validateRequest(ReviewValidation.updateReviewZodSchema), ReviewController.updateReview);
+
+router.delete('/:id', checkAuth(Role.PATIENT), ReviewController.deleteReview);
+
+
+
+
+export const ReviewRoutes = router;
